d842ff786206c0c147846eaef594b187b9ee6c9a
ssc1993 <18303093295@163.com>
2019-08-28 17:16:06 +0800

    书籍模块修改

--- pages/book/book.js
+++ pages/book/book.js
@@ -1,23 +1,79 @@
-// pages/book/book.js
-Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
+import {
+  BookModel
+} from "../../models/book.js"
 
-  },
+// 实例化BookModel
+const bookModel = new BookModel();
+
+Page({
 
   /**
-   * 组件的初始数据
+   * 页面的初始数据
    */
   data: {
+    // 定义数组
+    books:[]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    // 调取接口的书籍接口
+    bookModel.getBookList(2, 20).then(res => {
+      this.setData({
+        books:res.list
+      })
+      // console.log(res.list);
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
 
   },
 
   /**
-   * 组件的方法列表
+   * 生命周期函数--监听页面显示
    */
-  methods: {
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
 
   }
 })
