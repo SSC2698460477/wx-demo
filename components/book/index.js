30339ac37b7999bd64d81344a846196e5af27cd7
ssc1993 <18303093295@163.com>
2019-08-28 17:12:58 +0800

    开发书籍page

--- components/book/index.js
+++ components/book/index.js
@@ -18,6 +18,13 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    // 书籍封面的点击事件处理
+    onTap(event){
+      const bid = this.properties.book.id;
+      // console.log(bid);
+      wx.navigateTo({
+        url: `/pages/book-detail/book-detail?bid=${bid}`,
+      })
+    }
   }
 })
