c17cacb37105df232b58d1a0c67d991912b3d5f6
ssc1993 <18303093295@163.com>
2019-08-28 17:17:00 +0800

    修改player和team模块

--- pages/nbaTeam/nbaTeam.js
+++ pages/nbaTeam/nbaTeam.js
@@ -1,66 +1,77 @@
-// pages/nbaTeam/nbaTeam.js
+import {
+  NBAModel
+} from "../../models/nba.js"
+// 初始化
+const nbaModel = new NBAModel();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    teams:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    // 获取所有球队信息
+    nbaModel.getAllTeamList().then(res => {
+      this.setData({
+        teams:res
+      })
+      // console.log(res);
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
