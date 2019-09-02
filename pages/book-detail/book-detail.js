be46e60a2738024b718ee3d85b4ce1d5ce028f43
ssc1993 <18303093295@163.com>
2019-09-02 18:15:51 +0800

    详情页面的继续开发

--- pages/book-detail/book-detail.js
+++ pages/book-detail/book-detail.js
@@ -1,67 +1,89 @@
-// pages/book-detail/book-detail.js
+import {
+  BookModel
+} from "../../models/book.js"
+import {
+  StringUtil
+}from "../../util/stringUtil.js"
+
+// 实例化Bookmodel
+const bookModel = new BookModel();
+const stringUtil = new StringUtil();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    book: Object,
+    pubInfo:Object,
+    comments:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     const bid = options.bid;
-    console.log(bid);
+    console.log("书籍的id是：" + bid);
+    bookModel.getBookById(bid).then(res => {
+      // console.log(res);
+      const pubInfo = stringUtil.strToObj(res.bookInfo.pubInfo);
+      this.setData({
+        book:res.bookInfo,
+        pubInfo:pubInfo,
+        comments: res.bookShortComments
+      })
+     
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
