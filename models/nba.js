fef261c53482318891081aab015a0f068f180434
ssc1993 <18303093295@163.com>
2019-09-03 17:43:48 +0800

    增加接口

--- models/nba.js
+++ models/nba.js
@@ -10,6 +10,22 @@ class NBAModel extends HTTP {
       url: "api/nba/queryAllTeam"
     })
   }
+
+  // 根据teamId获取球队以及该球队球员信息
+  getTeamInfoById(teamId){
+    return this.request({
+      url:`api/nba/queryTeamById/${teamId}`,
+    })
+  }
+  // 根据teamId 查询球员信息列表
+  getPlayersByTeamId(teamId) {
+    return this.request({
+      url: "api/nba/queryPlayersByTeamId",
+      data: {
+        teamId: teamId
+      }
+    })
+  }
 }
 
 export {
