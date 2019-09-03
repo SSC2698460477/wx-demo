22e6178ccbdcc8546c9b42a93ae6c5a176bd6c0d
ssc1993 <18303093295@163.com>
2019-09-03 17:42:34 +0800

    修改team组件

--- components/team/index.js
+++ components/team/index.js
@@ -18,6 +18,11 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    onTap(event){
+      const teamId = this.data.team.teamId;
+      wx.navigateTo({
+        url: `/pages/nbaTeam-detail/nbaTeam-detail?teamId=${teamId}`,
+      })
+    }
   }
 })
