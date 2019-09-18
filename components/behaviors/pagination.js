bcdea1ab165c9a6602f84388060bc42bf5fd2f10
ssc1993 <18303093295@163.com>
2019-09-18 14:04:36 +0800

    代码优化

--- components/behaviors/pagination.js
+++ components/behaviors/pagination.js
@@ -2,7 +2,10 @@ const paginationBev = Behavior({
   // 需要用到的数据
   data: {
     dataArray: [],
-    total: null
+    total: null,
+    loading:false,
+    noneResult:false,
+    noneMore:false
   },
   // 需要提供的方法
   methods: {
@@ -19,21 +22,56 @@ const paginationBev = Behavior({
     },
     // 设置查询数据的总条数
     setTotal(total) {
-      this.data.total = total
+      this.data.total = total;
+      if(total == 0){
+        this.setData({
+          noneResult:true
+        })
+      }
     },
     // 判断是否还有数据需要加载
     hasMore() {
       if (this.data.dataArray.length >= this.data.total) {
-        console.log(this.data.total)
+        // 没有更多数据需要加载
+        this.setData({
+          noneMore:true
+        })
         return false;
       } else {
+        this.setData({
+          noneMore:false
+        })
         return true;
       }
     },
     // 初始化数据
     initialize() {
-      this.data.dataArray = [];
+      // this.data.dataArray = [];
+      this.setData({
+        dataArray:[],
+        loading:false,
+        noneResult:false,
+        noneMore:false
+      })
       this.data.total = null;
+    },
+    // 判断是否上锁
+    isLocked() {
+      return this.data.loading ? true : false
+    },
+    // 加锁
+    locked() {
+      // this.data.loading = true;
+      this.setData({
+        loading: true
+      })
+    },
+    // 释放锁
+    unLocked() {
+      // this.data.loading = false;
+      this.setData({
+        loading: false
+      })
     }
   }
 })
